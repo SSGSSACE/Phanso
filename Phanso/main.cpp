248a86e3a0efeac913736a8895ed74be74ed4a0f
SSGSSACE <quangbayhai@gmail.com>
2021-07-04 15:40:32 +0700

    July 4, 2021 3:40 PM

--- Phanso/main.cpp
+++ Phanso/main.cpp
@@ -9,6 +9,20 @@ int main()
     ps.Xuat();
     float re = ps.kq();
     std::cout << "Ket qua:" << ps.kq() << std::endl;
+    if (re>0)
+    {
+        std::cout << "Phan so duong" << std::endl;
+    }
+    else if (re<0)
+    {
+        std::cout << "Phan so am" << std::endl;
+    }
+    else
+    {
+        std::cout << "Phan so bang 0" << std::endl;
+    }
+    
+    
     system("pause");
     return 0;
 }
