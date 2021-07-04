8fbf3cc13662638aaf23408e672eb3ee2a96d573
SSGSSACE <quangbayhai@gmail.com>
2021-07-04 15:38:22 +0700

    July 4, 2021 3:38 PM

--- Phanso/main.cpp
+++ Phanso/main.cpp
@@ -7,8 +7,8 @@ int main()
     Phanso ps;
     ps.Nhap();
     ps.Xuat();
-    ps.kq();
-    std::cout << "Ket qua:" <<ps.kq() << std::endl;
+    float re = ps.kq();
+    std::cout << "Ket qua:" << ps.kq() << std::endl;
     system("pause");
     return 0;
 }
